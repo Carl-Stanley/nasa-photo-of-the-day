a226f884e1ac0d042430faab9852ef02127fd31c
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-07-30 22:41:14 -0700

    Added Components folder, added Axios to project. Added Header and Footer and imageRenderer files.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
+
+    
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
