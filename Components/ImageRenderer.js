2f6d55a2dea12694f2354f65a267125f3e37a595
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-07-31 16:57:02 -0700

    commit

--- Components/ImageRenderer.js
+++ Components/ImageRenderer.js
@@ -2,3 +2,18 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 
+export default function imageGetter (props) {
+return (
+
+
+
+    <div>
+
+        <h1>test</h1>
+
+    </div>
+)
+
+
+
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import imageGetter from "..Components/ImageRendered";
 
 function App() {
+  
+  
   return (
 
     
     <div className="App">
+
+      <imageGetter />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+
+     
     </div>
   );
 }
